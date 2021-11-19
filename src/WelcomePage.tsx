0213a486234664c597d593247c48ff3aea62ff1a
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-11-19 23:35:56 +0100

    add create user api call

--- src/WelcomePage.tsx
+++ src/WelcomePage.tsx
@@ -1,4 +1,25 @@
+import { useState, useRef } from "react";
+
 export const WelcomePage = () => {
+
+  // const [username, setUsername] = useState("")
+  const username = useRef(null);
+
+  const login = () => {
+    const name = username.current!
+    const uname = name['value']
+    console.log(uname)
+
+    fetch("https://api.newoncequiz.pl/api/users",{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name: uname})
+    })
+  }
+
+
   return (
     <div>
       <h1 className={"primary"}>Zagraj z innymi w newonce.play</h1>
@@ -7,8 +28,8 @@ export const WelcomePage = () => {
         ogóle w niej zrobić :P
       </p>
       <h2 className="label">Podaj imię</h2>
-      <input id="name" placeholder="Twoje imię"/>
-      <button className="primary">lecimy</button>
+      <input id="name" placeholder="Twoje imię" ref={username}/>
+      <button onClick={login} className="primary">lecimy</button>
     </div>
   );
 }
