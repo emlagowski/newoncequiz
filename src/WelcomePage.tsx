2595baee5b39fee034925d639cc77825f33bb6f0
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-11-19 21:51:39 +0100

    add input

--- src/WelcomePage.tsx
+++ src/WelcomePage.tsx
@@ -2,11 +2,12 @@ export const WelcomePage = () => {
   return (
     <div className="App">
       <h1 className={"primary"}>Zagraj z innymi w newonce.play</h1>
-      <p className={"primary grow"}>
+      <p className={"primary"}>
         Tu jest taki piękny opis co ta gra robi, dla kogo jest i co można w
         ogóle w niej zrobić :P
       </p>
-      <button>lecimy</button>
+      <h2 className="label">Podaj imię</h2>
+      <input id="name" placeholder="Twoje imię"/>
       <button className="primary">lecimy</button>
     </div>
   );
