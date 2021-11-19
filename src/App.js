845dc42fa3c8c83a6bfbc8295b0966fb17722c43
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-11-19 20:58:22 +0100

    initial styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className={"primary"}>Zagraj z innymi w newonce.play</h1>
+      <p className={"primary grow"}>Tu jest taki piękny opis co ta gra robi, dla kogo jest i co można w ogóle w niej zrobić :P</p>
       <button>lecimy</button>
       <button className="primary">lecimy</button>
     </div>
